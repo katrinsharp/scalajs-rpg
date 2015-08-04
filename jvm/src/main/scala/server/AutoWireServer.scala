6b462d8792ccc2f2debfe075da67d0fe7c568328
dave <dave.sugden@gmail.com>
2015-08-03 20:12:16 -0400

    upickle

--- jvm/src/main/scala/server/AutoWireServer.scala
+++ jvm/src/main/scala/server/AutoWireServer.scala
@@ -5,8 +5,8 @@ import shared.Api
 import scala.concurrent.ExecutionContext
 
 object AutowireServer extends autowire.Server[String, Reader, Writer] {
-  def read[Result: Reader](p: String) = read[Result](p)
-  def write[Result: Writer](r: Result) = write(r)
+  def read[Result: Reader](p: String) = upickle.default.read[Result](p)
+  def write[Result: Writer](r: Result) = upickle.default.write(r)
 
   def routes(implicit ex: ExecutionContext) = AutowireServer.route[Api](ApiImpl)
 }
